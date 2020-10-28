729c1afe3e58fe4cfc503de3ebe44bd10eee5f8d
Robert Darku <rdarku@gmail.com>
2020-10-28 14:03:52 -0400

    Update category done!!

---
+++ SpacePlace.Models/Categories/CategoryEdit.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SpacePlace.Models.Categories
+{
+    public class CategoryEdit
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+    }
+}
