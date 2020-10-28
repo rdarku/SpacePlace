6268c9c43485f7406b7cdaaf85276790118debd5
Robert Darku <rdarku@gmail.com>
2020-10-28 17:01:27 -0400

    Space amenities done

--- SpacePlace.Services/SpaceAmenityService.cs
+++ SpacePlace.Services/SpaceAmenityService.cs
@@ -1,13 +1,132 @@
-﻿using System;
+﻿using SpacePlace.Data;
+using SpacePlace.Models.SpaceAmenities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SpacePlace.Services
 {
     public class SpaceAmenityService
     {
+        public bool CreateSpaceAmenity(SpaceAmenityCreate model)
+        {
+            var newSpaceAmenity = new SpaceAmenity
+            {
+                AmenityId = model.AmenityId,
+                CreatedAt = DateTimeOffset.Now,
+                SpaceId = model.SpaceId
+            };
 
+            try
+            {
+                using (var ctx = new ApplicationDbContext())
+                {
+                    ctx.SpaceAmenities.Add(newSpaceAmenity);
+
+                    return ctx.SaveChanges() == 1;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+        }
+
+        public IEnumerable<SpaceAmenityListItem> GetAllSpaceAmmenities()
+        {
+            try
+            {
+                using (var ctx = new ApplicationDbContext())
+                {
+                    return ctx.SpaceAmenities
+                        .Select(s => new SpaceAmenityListItem
+                        {
+                            AmenityName = s.Amenity.Name,
+                            SpaceName = s.Space.Name,
+                            Id = s.Id
+                        }).ToList();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+
+        public SpaceAmenityDetails GetSpaceAmenityById(int id)
+        {
+            try
+            {
+                using (var ctx = new ApplicationDbContext())
+                {
+                    var spaceAmenity = ctx.SpaceAmenities.Where(s => s.Id == id)
+                        .FirstOrDefault();
+
+                    if (spaceAmenity == null) return null;
+
+                    return new SpaceAmenityDetails()
+                    {
+                        Id = spaceAmenity.Id,
+                        AmenityId = spaceAmenity.AmenityId,
+                        AmenityName = spaceAmenity.Amenity.Name,
+                        Description = spaceAmenity.Amenity.Description,
+                        SpaceId = spaceAmenity.SpaceId,
+                        SpaceName = spaceAmenity.Space.Name
+                    };
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+
+        public bool UpdateSpaceAmenity(SpaceAmenityEdit model)
+        {
+            try
+            {
+                using (var ctx = new ApplicationDbContext())
+                {
+                    var spaceAmenity = ctx.SpaceAmenities.Where(s => s.Id == model.Id)
+                        .FirstOrDefault();
+
+                    if (spaceAmenity == null) return false;
+
+                    spaceAmenity.SpaceId = model.SpaceId;
+                    spaceAmenity.AmenityId = model.AmenityId;
+
+                    return ctx.SaveChanges() == 1;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+        }
+
+        public bool DeleteSpaceAmenity(int id)
+        {
+            try
+            {
+                using (var ctx = new ApplicationDbContext())
+                {
+                    var spaceAmenity = ctx.SpaceAmenities.Where(s => s.Id == id)
+                        .FirstOrDefault();
+
+                    ctx.SpaceAmenities.Remove(spaceAmenity);
+
+                    return ctx.SaveChanges() == 1;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+        }
     }
 }
--- SpacePlace.WebAPI/Controllers/SpaceAmenityController.cs
+++ SpacePlace.WebAPI/Controllers/SpaceAmenityController.cs
@@ -1,13 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
+﻿using SpacePlace.Models.SpaceAmenities;
+using SpacePlace.Services;
 using System.Web.Http;
 
 namespace SpacePlace.WebAPI.Controllers
 {
     public class SpaceAmenityController : ApiController
     {
+        private readonly SpaceAmenityService _service = new SpaceAmenityService();
+
+        public IHttpActionResult Post([FromBody] SpaceAmenityCreate model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(_service.CreateSpaceAmenity(model));
+        }
+
+        public IHttpActionResult Get()
+        {
+            return Ok(_service.GetAllSpaceAmmenities());
+        }
+
+        public IHttpActionResult Get([FromUri] int id)
+        {
+            return Ok(_service.GetSpaceAmenityById(id));
+        }
+
+        public IHttpActionResult Put([FromBody] SpaceAmenityEdit model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(_service.UpdateSpaceAmenity(model));
+        }
+
+        public IHttpActionResult Delete([FromUri] int id)
+        {
+            return Ok(_service.DeleteSpaceAmenity(id));
+        }
     }
 }
