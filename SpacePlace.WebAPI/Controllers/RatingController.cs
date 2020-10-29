769d711c84726a112739cff6c2758ddbbdfe730b
lyankey <56731939+lyankey@users.noreply.github.com>
2020-10-29 09:44:01 -0400

    Add Rating Details - please review

--- SpacePlace.WebAPI/Controllers/RatingController.cs
+++ SpacePlace.WebAPI/Controllers/RatingController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SpacePlace.Models.Ratings;
+using SpacePlace.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,5 +11,38 @@ namespace SpacePlace.WebAPI.Controllers
 {
     public class RatingController : ApiController
     {
+        private readonly RatingService _service = new RatingService();
+
+        // post -- create
+        public IHttpActionResult Post(RatingCreate model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (_service.CreateRating(model))
+                    return Ok();
+
+                return InternalServerError();
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
+
+        // get -- list
+        public IHttpActionResult Get()
+        {
+            return Ok(_service.GetAllRatings());
+        }
+
+        // get -- by Id
+        public IHttpActionResult Get([FromUri] int id)
+        {
+            return Ok(_service.GetRatingById(id));
+        }
+
+        // Put -- update -- we are not allowing at this time
+        //Remove -- we are not allowing at this time
+
     }
 }
