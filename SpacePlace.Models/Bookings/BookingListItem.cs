4d2f7a9c1dc2a6d972442cc05b7d32e950e4343d
jacobschilders <jschilders@anderson.edu>
2020-10-29 06:25:32 -0400

    create bookinglistitem model, implement getallbookings service, create get methjod in bookingcontroller

--- SpacePlace.Models/Bookings/BookingListItem.cs
+++ SpacePlace.Models/Bookings/BookingListItem.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SpacePlace.Models.Bookings
 {
-    class BookingListItem
+    public class BookingListItem
     {
+        public int BookingId { get; set; }
+
+        public int SpaceId { get; set; }
+
+        public int RenterId { get; set; }
+
+        public string Duration { get; set; }
+
+        public string DurationUnit { get; set; }
+
+        [Display(Name ="Date Booked")]
+        public DateTimeOffset BookingDate { get; set; }
+
+        [Display(Name = "Start Time")]
+        public DateTimeOffset StartDate { get; set; }
+
+        [Display(Name = "End Time")]
+        public DateTimeOffset EndDate { get; set; }
     }
 }
---
+++ SpacePlace.WebAPI/Controllers/BookingController.cs
@@ -0,0 +1,43 @@
+﻿using SpacePlace.Models.Bookings;
+using SpacePlace.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SpacePlace.WebAPI.Controllers
+{
+    public class BookingController : ApiController
+    {
+        private readonly BookingService _service = new BookingService();
+
+        //Post -- Create
+        public IHttpActionResult Post(BookingCreate model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (_service.CreateBooking(model))
+                    return Ok();
+                return InternalServerError();
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
+
+        //Get -- List
+        public IHttpActionResult Get()
+        {
+            return Ok(_service.GetAllBookings());
+        }
+
+        //Get -- By ID
+
+        //Put -- Update
+
+        //Delete -- Remove
+    }
+}
